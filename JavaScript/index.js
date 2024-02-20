8a471a52806d77d054f68bfdbbbb574d371350b3
malsebasmal <antoniomaldonadoestudio@gmail.com>
2024-02-20 15:32:37 -0500

    optimization: index.js | Changes names of variables and delete a few functions and changes others

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,50 +1,23 @@
 //Inputs disponibles:
 let options = ["rock", "paper", "scissors"]
 let selectionUser = prompt(`Your options are these ${options}`)
+let selectionRandomComputer = options[Math.floor(Math.random() * options.length)]
 
-//Election for the User
-function choiceUser() {
-    if (selectionUser == options[0]) {
-        return alert(`You are selection ${options[0]}`)
-    } else if (selectionUser == options[1]) {
-        return alert(`You are selection ${options[1]}`)
-    } else if (selectionUser == options[2]) {
-        return alert(`You are selection ${options[2]}`)
-    } else {
-        return alert("Please, you are need selection one valides option")
-    }
-}
-choiceUser()
-
-//Election for the Computer
-    //Variable for the random number
-let randomNumberComputer = Math.floor(Math.random() * options.length)
-function showChoiceComputer() {
-    let selectionComputer
-
-    if (randomNumberComputer === 0) {
-        selectionComputer = `The computer selection ${options[0]}`
-    } else if (randomNumberComputer === 1) {
-        selectionComputer = `The computer selection ${options[1]}`
-    } else if (randomNumberComputer === 2) {
-        selectionComputer = `The computer selection ${options[2]}`
-    }
-
-    return alert(selectionComputer)
-}
-showChoiceComputer()
+//Show the election
+    //User
+alert(`You are selection ${selectionUser}`)
+    //Computer
+alert(`The computer selection ${selectionRandomComputer}`)
 
 // Battle
 function battleUserComputer() {
-    if ((selectionUser === "rock" && randomNumberComputer === 0) ||
-        (selectionUser === "paper" && randomNumberComputer === 1) ||
-        (selectionUser === "scissors" && randomNumberComputer === 3)) {
+    if (selectionUser === selectionRandomComputer) {
         return alert("Tie .-.")
-    } else if (selectionUser === "rock" && randomNumberComputer === 2) {
+    } else if (selectionUser === "rock" && selectionRandomComputer === "paper") {
         return alert("You win!")
-    } else if (selectionUser === "paper" && randomNumberComputer === 0) {
+    } else if (selectionUser === "paper" && selectionRandomComputer === "rock") {
         return alert("You win!")
-    } else if (selectionUser === "scissors" && randomNumberComputer === 1) {
+    } else if (selectionUser === "scissors" && selectionRandomComputer === "scissors") {
         return alert("You win!")
     } else {
         return alert("You lose...")
